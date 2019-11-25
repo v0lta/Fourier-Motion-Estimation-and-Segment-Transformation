Rotation fix.

cells/registration_cell.py
@@ -102,20 +102,21 @@ class RegistrationCell(torch.nn.Module):
 
         if self.rotation:
             cent = compute_2d_centroid(pred_img)
-            rot_pred_img = fft_rotation(pred_img, rz)
-            rot_pred_img_cent = compute_2d_centroid(rot_pred_img)
-            displacement = cent - rot_pred_img_cent
-            displacement = displacement/(64. + 8)
-            rot_trans_pred_img = fft_translation(rot_pred_img, displacement[:, 1], displacement[:, 0])
+            # move to the center
+            displacement_x = (32 - cent[:, 1])/64.
+            displacement_y = (32 - cent[:, 0])/64.
+
+            cent_img = fft_translation(pred_img, displacement_x, displacement_y)
+            # rotate
+            rot_pred_img = fft_rotation(cent_img, rz)
+            # move back
+            rot_trans_pred_img = fft_translation(rot_pred_img, -displacement_x, -displacement_y)
 
             debug = False
             if debug:
-                print('rz', rz)
-                print('c1', cent, 'cr', rot_pred_img_cent, 'd', displacement)
-                final_cent = compute_2d_centroid(pred_img)
-                print('final_cent', final_cent)
-                print('error', cent-final_cent)
-                plt.imshow(torch.cat([pred_img, rot_pred_img, rot_trans_pred_img], -1).cpu()[0, :, :])
+                print(cent, displacement_x, displacement_y)
+                print('cent', cent)
+                plt.imshow(torch.cat([cent_img, rot_pred_img, rot_trans_pred_img], -1).cpu()[0, :, :])
                 plt.show()
 
             pred_img = torch.clamp(rot_trans_pred_img, 0., 1.)
@@ -231,8 +232,8 @@ class VelocityEstimationCell(torch.nn.Module):
 
 if __name__ == '__main__':
     it = MovingMNISTAdvancedIterator(initial_velocity_range=(0.0, 0.0),
-                                     rotation_angle_range=(2.5, 2.5),
-                                     global_rotation_angle_range=(2.5, 2.5))
+                                     rotation_angle_range=(5, 5),
+                                     global_rotation_angle_range=(5, 5))
     time = 7
     seq_np, motion_vectors = it.sample(5, time)
     seq_np = seq_np/255.

train_reg_gru.py
@@ -8,14 +8,14 @@ from cells.registration_cell import RegistrationCell, VelocityEstimationCell, Ga
 from moving_mnist_pp.movingmnist_iterator import MovingMNISTAdvancedIterator
 from torch.utils.tensorboard import SummaryWriter
 
-rotation = 2.5
-it = MovingMNISTAdvancedIterator(initial_velocity_range=(1.0, 2.0),
+rotation = 4
+it = MovingMNISTAdvancedIterator(initial_velocity_range=(1.0, 3.6),
                                  rotation_angle_range=(-rotation, rotation),
-                                 global_rotation_angle_range=(rotation, rotation))
+                                 global_rotation_angle_range=(-rotation, rotation))
 batch_size = 200
 time = 10
-context_time = 5
-pred_time = 5
+context_time = 4
+pred_time = 6
 state_size = 200
 cell = RegistrationCell(state_size=state_size, rotation=True).cuda()
 # cell = VelocityEstimationCell(cnn_depth_lst=[10, 10, 10, 10], state_size=state_size).cuda()
@@ -61,7 +61,7 @@ if 1:
                 prediction_video_lst.append(pimg)
 
             pred_vid = torch.stack(prediction_video_lst, dim=0)
-            pred_vid = torch.clamp(pred_vid, 0.0, 1.0)
+            # pred_vid = torch.clamp(pred_vid, 0.0, 1.0)
             loss = criterion(pred_vid, prediction)
 
             # compute gradients

util/rotation_translation_pytorch.py
@@ -139,7 +139,7 @@ def fft_rotation(image, theta):
     _, row_no, col_no = image.shape
     # Restrict theta to at most 0.25pi in order to prevent the tan from blowing up.
     # theta = torch.tanh(-theta)*np.pi*0.25
-    theta = torch.clamp(theta, -2.5*(2*np.pi)/360, 2.5*(2*np.pi)/360)
+    theta = torch.clamp(theta, -4.*(2*np.pi)/360, 4.*(2*np.pi)/360)
     # print('clamp', theta)
     a = torch.tan(theta/2)
     b = -torch.sin(theta)